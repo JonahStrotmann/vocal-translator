07b454cc12e3d35ceac01b392832db8125ecf513
Jonah Strotmann <39196875+JonahStrotmann@users.noreply.github.com>
2021-02-02 14:28:52 +0100

    you can now type multiple sentences and exit the program with exit

--- vocal-translator.py
+++ vocal-translator.py
@@ -1,25 +1,32 @@
-sentence = input("please insert sentence here: ")
+import sys
 
-sentence = sentence.replace("o", "oob") 
-sentence = sentence.replace("a", "oob") 
-sentence = sentence.replace("e", "oob") 
-sentence = sentence.replace("i", "oob") 
-sentence = sentence.replace("u", "oob") 
 
-sentence = sentence.replace("O", "oob") 
-sentence = sentence.replace("A", "oob") 
-sentence = sentence.replace("E", "oob") 
-sentence = sentence.replace("I", "oob") 
-sentence = sentence.replace("U", "oob") 
+def translate_sentence(sentence):
+    sentence = sentence.replace("o", "oob") 
+    sentence = sentence.replace("a", "oob") 
+    sentence = sentence.replace("e", "oob") 
+    sentence = sentence.replace("i", "oob") 
+    sentence = sentence.replace("u", "oob") 
 
-sentence = sentence.replace("ö", "ööb") 
-sentence = sentence.replace("ä", "ööb") 
-sentence = sentence.replace("ü", "ööb")
+    sentence = sentence.replace("O", "oob") 
+    sentence = sentence.replace("A", "oob") 
+    sentence = sentence.replace("E", "oob") 
+    sentence = sentence.replace("I", "oob") 
+    sentence = sentence.replace("U", "oob") 
 
-sentence = sentence.replace("Ö", "ööb") 
-sentence = sentence.replace("Ä", "ööb") 
-sentence = sentence.replace("Ü", "ööb")
+    sentence = sentence.replace("ö", "ööb") 
+    sentence = sentence.replace("ä", "ööb") 
+    sentence = sentence.replace("ü", "ööb")
 
-print(sentence)
+    sentence = sentence.replace("Ö", "ööb") 
+    sentence = sentence.replace("Ä", "ööb") 
+    sentence = sentence.replace("Ü", "ööb")
+    
+    return sentence
 
-input('Press enter to exit')
+while True:
+    sentence = input("please insert sentence here: ")
+    if(sentence == "exit"):
+        sys.exit(0)
+    translated_sentence = translate_sentence(sentence)
+    print(translated_sentence)
